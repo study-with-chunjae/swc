a17118e6b62f616cb0e26b73a0e0f298ece307b7
Choisarang <sarang1126611@naver.com>
2024-12-06 17:28:12 +0900

    main ui

---
+++ src/main/java/net/fullstack7/swc/controller/DummyController.java
@@ -0,0 +1,47 @@
+package net.fullstack7.swc.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/dummy")
+public class DummyController {
+
+    @GetMapping("/main/main")
+    public String main() {
+        return "main/main";
+    }
+
+    @GetMapping("/main/main2")
+    public String I() {
+        return "main/main2";
+    }
+
+    @GetMapping("/main/layout")
+    public String layout() {
+        return "main/layout";
+    }
+
+    @GetMapping("/main/layout2")
+    public String layout2() {
+        return "main/layout2";
+    }
+
+    @GetMapping("/todo/mylist")
+    public String myllist() {
+        return "todo/mylist";
+    }
+
+    @GetMapping("/todo/view")
+    public String view() {
+        return "todo/view";
+    }
+
+    @GetMapping("/todo/regist")
+    public String regist() {
+        return "todo/regist";
+    }
+}
