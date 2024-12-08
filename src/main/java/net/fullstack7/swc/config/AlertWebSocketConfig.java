dc303bd09dc08d507e60da9f00a4cc6594b48182
soojin <ssj1737@gmail.com>
2024-12-08 21:19:23 +0900

    alert확인

--- src/main/java/net/fullstack7/swc/config/AlertWebSocketConfig.java
+++ src/main/java/net/fullstack7/swc/config/AlertWebSocketConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class AlertWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/queue");
