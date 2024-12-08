e2f747c73be0958a945d3e68a3a34e67a3720910
[kangkamchan] <[topthanks@naver.com]>
2024-12-08 23:38:22 +0900

    kc/0011
    좋아요 RestController 까지 만듦 이제 페이지에 넣기만 하면됨

--- src/main/java/net/fullstack7/swc/repository/ThumbUpRepository.java
+++ src/main/java/net/fullstack7/swc/repository/ThumbUpRepository.java
@@ -1,7 +1,12 @@
 package net.fullstack7.swc.repository;
 
+import net.fullstack7.swc.domain.Member;
+import net.fullstack7.swc.domain.Post;
 import net.fullstack7.swc.domain.ThumbUp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ThumbUpRepository extends JpaRepository<ThumbUp, Integer> {
+    Optional<ThumbUp> findByPostAndMember(Post post, Member member);
 }
--- src/main/java/net/fullstack7/swc/service/ThumbUpServiceIf.java
+++ src/main/java/net/fullstack7/swc/service/ThumbUpServiceIf.java
@@ -1,4 +1,8 @@
 package net.fullstack7.swc.service;
 
+import net.fullstack7.swc.domain.ThumbUp;
+
 public interface ThumbUpServiceIf {
+    ThumbUp addThumbUp (Integer postId, String memberId);
+    boolean removeThumbUp(Integer postId, String memberId);
 }
