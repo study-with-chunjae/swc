4373ad53ddbb11baa571ea6564bb50d18cf4841f
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2024-12-09 06:21:51 +0900

    loginGo

---
+++ src/main/resources/static/assets/js/sign/signIn.js
@@ -0,0 +1,75 @@
+document.getElementById("signinForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const response = await fetch("/sign/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        memberId: document.getElementById("signinId").value,
+        pwd: document.getElementById("signinPwd").value,
+      }),
+    });
+
+    if (response.ok) {
+      const token = await response.text();
+      const isTempPassword = response.headers.get("X-Temp-Password");
+      console.log("임시 비밀번호 여부:", isTempPassword);
+
+      document.cookie = `accessToken=${token}; path=/; max-age=604800; secure`;
+
+      if (isTempPassword === "true") {
+        console.log("비밀번호 변경 페이지로 이동");
+        window.location.href = "/sign/forgotPasswordChange";
+      } else {
+        document.getElementById("signinForm").reset();
+        document.getElementById("memberInfo").style.display = "block";
+        location.href = "/post/main";
+      }
+    } else {
+      const errorMessage = await response.text();
+      alert(errorMessage);
+      document.getElementById("signinError").style.display = "block";
+      document.getElementById("signinError").textContent = errorMessage;
+    }
+  } catch (error) {
+    alert(error.message);
+    document.getElementById("signinError").style.display = "block";
+    document.getElementById("signinError").textContent = error.message;
+  }
+});
+
+// 로그인 스크립트 끗
+
+// 비동기 방식으로 토큰 전송
+async function sendTokenAsync() {
+  try {
+    const token = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("accessToken="))
+      ?.split("=")[1];
+
+    if (!token) {
+      alert("토큰이 없습니다. 로그인이 필요합니다.");
+      return;
+    }
+
+    const response = await fetch("/sign/cookie-test", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `accessToken=${token}`,
+    });
+
+    if (response.ok) {
+      const result = await response.text();
+      alert("토큰 전송 성공\n" + result);
+    } else {
+      throw new Error("토큰 전송 실패");
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+}
