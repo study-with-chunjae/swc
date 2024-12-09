07a16f9ee674b79ee25089a76baf20690780bc16
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2024-12-10 00:57:12 +0900

    ll

--- src/main/java/net/fullstack7/swc/config/SecurityConfig.java
+++ src/main/java/net/fullstack7/swc/config/SecurityConfig.java
@@ -23,10 +23,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            // .authorizeHttpRequests(auth -> auth
-            //     .requestMatchers("/sign/**", "/oauth2/**", "/").permitAll()
-            //     .anyRequest().authenticated()
-            // )
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
             .oauth2Login(oauth2 -> oauth2
                 .userInfoEndpoint(userInfo -> userInfo
                     .userService(customOAuth2UserService)
